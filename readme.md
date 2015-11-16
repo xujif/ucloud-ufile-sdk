b961c6861290df8506f09d8e3f24353762e3c7c9
xujif <i@xujif.com>
2015-11-16 11:17:24 +0800

    修改测试方法和readme

--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 #ucloud ufile sdk
 base on https://docs.ucloud.cn/api-docs/ufile-api/ 
+#usage
+    $sdk = new UfileSdk('storage','ap_pub_key','ap_pub_secret');
+    $sdk->put('text.txt',"content");
+    $sdk->putFile('text.txt',"/path/to/yourfile");
+    $contents = $sdk->get('dd');
+    $exists =  $sdk->exists('dd222');
+    $size = $sdk->delete('dd');
+或者直接查看tests/sdkTest.php
--- tests/sdkTest.php
+++ tests/sdkTest.php
@@ -2,42 +2,39 @@
 
 use Xujif\UcloudUfileSdk\UfileSdk;
 
-function dd(){
+function dd() {
 	var_dump(func_get_args());die();
 }
 
-class sdkTest extends  PHPUnit_Framework_TestCase
-{
-    protected $sdk;
-    public function setUp(){
-        $this->sdk = new UfileSdk('storage321','***','*****');
-    }
-    public function testPutFile()
-    {
-        list($ret,$code) = $this->sdk->put('text.txt',__DIR__.'/text.txt');
-        $this->assertEquals($code,200);
-    }
-    public function testPut()
-    {
-        list($ret,$code) = $this->sdk->put('dd','sdfsdfsdfsfsdf');
-        $this->assertEquals($code,200);
-    }
-    public function testGet(){
-        $contents = $this->sdk->get('dd');
+class sdkTest extends PHPUnit_Framework_TestCase {
+	protected $sdk;
+	public function setUp() {
+		$this->sdk = new UfileSdk('storage321', '***', '*****');
+	}
+	public function testPutFile() {
+		list($ret, $code) = $this->sdk->putFile('text.txt', __DIR__ . '/text.txt');
+		$this->assertEquals($code, 200);
+	}
+	public function testPut() {
+		list($ret, $code) = $this->sdk->put('dd', 'sdfsdfsdfsfsdf');
+		$this->assertEquals($code, 200);
+	}
+	public function testGet() {
+		$contents = $this->sdk->get('dd');
 
-        $this->assertEquals($contents,'sdfsdfsdfsfsdf');
-    }
-    public function testExists(){
-        $exists =  $this->sdk->exists('dd222');
-        $this->assertEquals($exists,false);
-    }
+		$this->assertEquals($contents, 'sdfsdfsdfsfsdf');
+	}
+	public function testExists() {
+		$exists = $this->sdk->exists('dd222');
+		$this->assertEquals($exists, false);
+	}
 
-    public function testSize(){
-        $size = $this->sdk->size('dd');
-        $this->assertEquals($size,14);
-    }
-    //mime 不准
-    // public function testMime(){
-    //     $mime = $this->sdk->mime('text.txt');
-    // }
+	public function testSize() {
+		$size = $this->sdk->size('dd');
+		$this->assertEquals($size, 14);
+	}
+	//mime 不准
+	// public function testMime(){
+	//     $mime = $this->sdk->mime('text.txt');
+	// }
 }
